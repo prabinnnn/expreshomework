57949a0259b09091cff77ebba3cfc77ea1297ae4
prabinnnn <prabinbhandari120@gmail.com>
2024-01-29 16:17:03 +0545

    add index

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,16 @@ const adminrouter = require("./admin.route");
 const indexuser = "/ap1/v1";
 const indexadmin = "/ap2/v2";
 
-const checkrole = (userRoles, adminRoles) => {
+const checkrole = (adminRoles) => {
   return (req, res, next) => {
-    const result = userRoles.includes(adminRoles);
+    adminRoles = [adminRoles];
+    const userRoles = req.body.role || req.header.role;
+    const result = userRoles.filter((role) => adminRoles.includes(role));
     if (!result) throw new new Error({ msg: "permssion denied" })();
+    next();
   };
 };
-router.get("/user/:id", checkrole("user", "admin"), (req, res, next) => {
-  if (req.header.includes(userRoles)) next("user");
-  else {
-    next("admin");
-  }
-});
+
 router.get("/user", checkrole("user"), (res, next, req) => {
   try {
     res.send({ msg: "access denied" });
@@ -23,7 +21,7 @@ router.get("/user", checkrole("user"), (res, next, req) => {
     next(e);
   }
 });
-router.get("/user", checkrole(["admin"]), (res, next, req) => {
+router.get("/user", checkrole("admin"), (res, next, req) => {
   try {
     res.send({ msg: "accessed" });
   } catch (e) {
