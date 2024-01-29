d6f200ce9b7eca03972e8e407f1bd93a1d65b473
prabinnnn <prabinbhandari120@gmail.com>
2024-01-29 16:11:11 +0545

    add index

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,17 @@ const indexadmin = "/ap2/v2";
 
 const checkrole = (userRoles, adminRoles) => {
   return (req, res, next) => {
-    const userroles = Array.isArray(userRoles);
-    const sysroles = Array.isArray(adminRoles);
-    const result = userroles.filter((role) => sysroles.includes(role));
+    const result = userRoles.includes(adminRoles);
     if (!result) throw new new Error({ msg: "permssion denied" })();
   };
 };
-// async function checkrole(userRoles, adminRoles, req) {
-//   await checkrole(req.header);
-//   const userroles = Array.isArray(userRoles);
-//   const sysroles = Array.isArray(adminRoles);
-//   const result = userroles.filter((role) => sysroles.includes(role));
-//   if (!result) throw new new Error({ msg: "permssion denied" })();
-// }
-// const userroles = req.header.userRoles;
-// const adminroles = req.header.adminRoles;
-// if (userroles == "user") {
-//   throw new Error("Access denied. User token missing.");
-// } else if (adminroles == "admin") {
-//   res.json({ msg: "admin" });
-//   next("admin");
-// } else {
-//   res.json({ msg: "error" });
-// }
-router.get("/user/:id", checkrole(["user"], ["admin"]), (req, res, next) => {
+router.get("/user/:id", checkrole("user", "admin"), (req, res, next) => {
   if (req.header.includes(userRoles)) next("user");
   else {
     next("admin");
   }
 });
-router.get("/user", checkrole(["user"]), (res, next, req) => {
+router.get("/user", checkrole("user"), (res, next, req) => {
   try {
     res.send({ msg: "access denied" });
   } catch (e) {
